ce1a2e9c997948df58bf60fdeeb0ac29d6cb1241
Maryna Longnickel <maryna.longnickel@gmail.com>
2025-06-16 21:27:14 -0500

    wip

---
+++ assignment1.js
@@ -0,0 +1,36 @@
+// Funny story variables
+const heroName = "Detective Waffles";
+const sidekick = "Slappy the Squirrel";
+let action = "slipped on a banana peel";
+let object = "giant rubber chicken";
+let villain = "Dr. Pickle";
+let place = "a donut shop";
+let twist = "they accidentally formed a boy band and went on tour";
+
+// Construct the story
+let story = `
+One day, ${heroName} and their trusty sidekick ${sidekick} were chasing ${villain} through ${place}.
+Suddenly, ${heroName} ${action} while holding a ${object}.
+Everyone gasped, but then ${twist}.
+It was the weirdest Tuesday ever.
+`;
+
+console.log("--- Funny Story ---");
+console.log(story);
+
+// Change values for a new version
+action = "accidentally launched a taco into space";
+object = "flaming ukulele";
+villain = "The Toastmaster";
+place = "a cheese museum";
+twist = "but then they baked a croissant so perfect it caused world peace";
+
+story = `
+One day, ${heroName} and their trusty sidekick ${sidekick} were chasing ${villain} through ${place}.
+Suddenly, ${heroName} ${action} while holding a ${object}.
+Everyone gasped, but then ${twist}.
+It was the weirdest Tuesday ever.
+`;
+
+console.log("--- Different Versoin of the Story ---");
+console.log(story);
---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Script");
+
+// str
+let firstName = 'Marina';
+let lastName = 'Longnickel'
+
+// num
+let age = 23;
+let mySize = 1;
+
+// bool
+let isStudent = false;
+
+console.log(firstName);
+console.log(age, mySize);
+
+// concatenation
+console.log("My name is " + firstName + " " + lastName);
+
+// operations
+
+let n1 = 10;
+let n2 = 2;
+
+let addition = n1 + n2;
+let subtraction = n1 - n2;
+let multiplication = n1 * n2;
+let division = n1 / n2;
+
+console.log("Addition is: " + addition);
+console.log("Subtraction is: " + subtraction);
+console.log("Multiplication is: " + multiplication);
+console.log("Division is: " + division);
+
+let r = 3;
+const pi = 3.14;
+let a = pi * r**2
+
+console.log("The area of a circle with a radius " + r + " is " + a)
